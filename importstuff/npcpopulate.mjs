da048ea0fda33cb811244fe746fe10980b0a9a2c
Supinic <supinic@protonmail.com>
2023-04-16 18:12:16 +0200

    fixed erroneously broken NPC population script
    also changed to `.mjs` so it can be easily run from CLI

--- importstuff/npcpopulate.mjs
+++ importstuff/npcpopulate.mjs
@@ -35,15 +35,15 @@ fs.createReadStream("./importstuff/wiki_monster_data.csv")
 			return;
 		}
 
-		const example = {
-			name: "Sotetseg",
-			image: "Sotetseg.png",
-			version: "",
-			versionNumber: 0,
-			combat: 995,
-			stats: { amagic: 0, arange: 0, att: 250, attbns: 0, dcrush: 70, def: 200, dmagic: 30, drange: 150, dslash: 70, dstab: 70, hitpoints: 4000, mage: 250, mbns: 0, range: 0, rngbns: 0, str: 250, strbns: 48 },
-			attributes: []
-		};
+		// const example = {
+		// 	name: "Sotetseg",
+		// 	image: "Sotetseg.png",
+		// 	version: "",
+		// 	versionNumber: 0,
+		// 	combat: 995,
+		// 	stats: { amagic: 0, arange: 0, att: 250, attbns: 0, dcrush: 70, def: 200, dmagic: 30, drange: 150, dslash: 70, dstab: 70, hitpoints: 4000, mage: 250, mbns: 0, range: 0, rngbns: 0, str: 250, strbns: 48 },
+		// 	attributes: []
+		// };
 
 		// parse image name
 		let filename = row.image;
