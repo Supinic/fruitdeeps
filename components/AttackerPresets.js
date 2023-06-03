fa3107b6758ca8ad9a1ca9c99703cc7e49a73b66
Supinic <supinic@protonmail.com>
2023-06-03 13:04:47 +0200

    bundled the `Dropdown` component back into `AttackerPresets` tab

--- components/AttackerPresets.js
+++ components/AttackerPresets.js
@@ -2,7 +2,24 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 import Player from "../lib/Player.js";
-import { PresetDropdown } from "./PresetDropdown.js";
+import items from "../public/assets/items.json" assert { type: "json" };
+import { presets } from "./builtin-presets.js";
+import searchFilter from "../lib/itemFinder.js";
+
+for (const item of presets) {
+	if (!item.equipmentIdentifiers) {
+		continue;
+	}
+
+	item.equipment = item.equipmentIdentifiers.map(i => {
+		const [result] = searchFilter(i, items);
+		if (!result) {
+			throw new Error(`No item found: ${i}`);
+		}
+
+		return result;
+	});
+}
 
 export class AttackerPresets extends Component {
 	static propTypes = {
@@ -11,11 +28,62 @@ export class AttackerPresets extends Component {
 		setTab: PropTypes.func
 	};
 
+	constructor (props) {
+		super(props);
+
+		this.elements = presets.map((i, ind) => {
+			if (i.separator) {
+				return (
+					<br key={ind} />
+				);
+			}
+
+			return (
+				<button key={ind} onClick={() => this.changeSetup(ind)}>{i.name}</button>
+			);
+		});
+	}
+
+	changeSetup (index) {
+		const player = this.props.player;
+		const preset = presets[index];
+		if (preset.name === "Clear") {
+			player.clearEquipment();
+			player.clearPrayers();
+			player.clearBoosts();
+			player.clearCustomBonuses();
+		}
+
+		if (Array.isArray(preset.equipment)) {
+			player.clearEquipment();
+			for (const item of preset.equipment) {
+				player.equip(item);
+			}
+		}
+
+		if (Array.isArray(preset.prayers)) {
+			player.clearPrayers();
+			for (const prayer of preset.prayers) {
+				player.selectPrayer(prayer);
+			}
+		}
+
+		if (Array.isArray(preset.boosts)) {
+			player.clearBoosts();
+			for (const boost of preset.boosts) {
+				player.addBoost(boost);
+			}
+		}
+
+		this.props.setPlayer(player.minimize());
+		this.props.setTab(0);
+	}
+
 	render () {
 		return (
 			<div className="highlight-section flex-container-vertical">
 				<h3>Apply setup preset</h3>
-				<PresetDropdown player={this.props.player} setTab={this.props.setTab} setPlayer={this.props.setPlayer} />
+				{...this.elements}
 			</div>
 		);
 	}
--- components/builtin-presets.js
+++ components/builtin-presets.js
@@ -6,9 +6,9 @@ export const presets = [
 		separator: true
 	},
 	{
-		name: "Max melee (Scythe)",
-		equipmentIds: [
-			22325, // Scythe of Vitur
+		name: "Max Scythe",
+		equipmentIdentifiers: [
+			"Scythe of Vitur",
 			"Torva full helm",
 			"Torva platebody",
 			"Torva platelegs",
@@ -20,8 +20,38 @@ export const presets = [
 		]
 	},
 	{
+		name: "Mid Fang",
+		equipmentIdentifiers: [
+			"Osmumten's fang",
+			"Dragon defender",
+			"Neitiznot faceguard",
+			"Bandos chestplate",
+			"Bandos tassets",
+			"Amulet of torture",
+			"Fire cape",
+			"Ferocious gloves",
+			"Primordial boots",
+			"Berserker ring (i)"
+		]
+	},
+	{
+		name: "Low Tentacle",
+		equipmentIdentifiers: [
+			"Abyssal tentacle",
+			"Dragon defender",
+			"Helm of neitiznot",
+			"Fighter torso",
+			"Obsidian platelegs",
+			"Amulet of fury",
+			"Fire cape",
+			"Barrows gloves",
+			"Dragon boots",
+			"Berserker ring (i)"
+		]
+	},
+	{
 		name: "Dharok",
-		equipmentIds: [
+		equipmentIdentifiers: [
 			"Dharok's greataxe",
 			"Dharok's platebody",
 			"Dharok's platelegs",
@@ -34,19 +64,31 @@ export const presets = [
 		]
 	},
 	{
+		name: "Verac",
+		equipmentIdentifiers: [
+			"Verac's flail",
+			"Verac's brassard",
+			"Verac's plateskirt",
+			"Verac's helm",
+			"Amulet of torture",
+			"Infernal cape",
+			"Ferocious gloves",
+			"Primordial boots",
+			"Berserker ring (i)"
+		]
+	},
+	{
 		separator: true
 	},
 	{
-		name: "Max range (Tbow)",
-		boosts: ["Ranging"],
-		prayers: ["Rigour"],
-		equipmentIds: [
+		name: "Max Tbow",
+		equipmentIdentifiers: [
 			"Twisted bow",
-			11212, // Dragon arrows
+			"Dragon arrow",
 			"Masori mask (f)",
 			"Masori body (f)",
 			"Masori chaps (f)",
-			22109, // Ava's assembler
+			"Ava's assembler",
 			"Pegasian boots",
 			"Zaryte vambraces",
 			"Necklace of anguish",
@@ -54,22 +96,124 @@ export const presets = [
 		]
 	},
 	{
+		name: "Max Bpipe",
+		equipmentIdentifiers: [
+			"Toxic blowpipe (dragon)",
+			"Masori mask (f)",
+			"Masori body (f)",
+			"Masori chaps (f)",
+			"Ava's assembler",
+			"Pegasian boots",
+			"Zaryte vambraces",
+			"Necklace of anguish",
+			"Archers ring (i)"
+		]
+	},
+	{
+		name: "Max Zcb",
+		equipmentIdentifiers: [
+			"Zaryte crossbow",
+			"Ruby dragon bolts (e)",
+			"Masori mask (f)",
+			"Masori body (f)",
+			"Masori chaps (f)",
+			"Ava's assembler",
+			"Pegasian boots",
+			"Zaryte vambraces",
+			"Necklace of anguish",
+			"Archers ring (i)"
+		]
+	},
+	{
+		name: "Mid Bpipe",
+		equipmentIdentifiers: [
+			"Toxic blowpipe (amethyst)",
+			"Armadyl helmet",
+			"Armadyl chestplate",
+			"Armadyl chainskirt",
+			"Ava's assembler",
+			"Pegasian boots",
+			"Barrows gloves",
+			"Necklace of anguish",
+			"Archers ring (i)"
+		]
+	},
+	{
+		name: "Low Rcb",
+		equipmentIdentifiers: [
+			"Rune crossbow",
+			"Ruby bolts (e)",
+			"Book of law",
+			"Karil's coif",
+			"Karil's leathertop",
+			"Karil's leatherskirt",
+			"Ava's accumulator",
+			"Shayzien boots (5)",
+			"Barrows gloves",
+			"Amulet of fury",
+			"Archers ring (i)"
+		]
+	},
+	{
 		separator: true
 	},
 	{
-		name: "Max mage (Shadow)",
-		boosts: ["Saturated heart"],
-		prayers: ["Augury"],
-		equipmentIds: [
-			27275, // Tumeken's shadow (Charged)
-			21795, // Imbued Zamorak cape
-			19544, // Tormented bracelet
-			12002, // Occult necklace
-			21018, // Ancestral hat
-			21021, // Ancestral top
-			21024, // Ancestral bottom
+		name: "Max Shadow",
+		equipmentIdentifiers: [
+			"Tumeken's shadow",
+			"Imbued zamorak cape",
+			"Tormented bracelet",
+			"Occult necklace",
+			"Ancestral hat",
+			"Ancestral robe top",
+			"Ancestral robe bottom",
 			"Seers ring (i)",
-			13235 // Eternal boots
+			"Eternal boots"
+		]
+	},
+	{
+		name: "Max Sang",
+		equipmentIdentifiers: [
+			"Sanguinesti staff",
+			"Elidinis' ward (f)",
+			"Imbued zamorak cape",
+			"Tormented bracelet",
+			"Occult necklace",
+			"Ancestral hat",
+			"Ancestral robe top",
+			"Ancestral robe bottom",
+			"Seers ring (i)",
+			"Eternal boots"
+		]
+	},
+	{
+		name: "Mid Trident",
+		equipmentIdentifiers: [
+			"Trident of the swamp",
+			25985, // "Elidinis' ward",
+			"Imbued zamorak cape",
+			"Tormented bracelet",
+			"Occult necklace",
+			"Ahrim's hood",
+			"Ahrim's robetop",
+			"Ahrim's robeskirt",
+			"Seers ring (i)",
+			"Eternal boots"
+		]
+	},
+	{
+		name: "Low Trident",
+		equipmentIdentifiers: [
+			"Trident of the seas",
+			"Book of darkness",
+			2414, // "Zamorak cape",
+			"Barrows gloves",
+			"Occult necklace",
+			"Mystic hat",
+			"Mystic robe top",
+			"Mystic robe bottom",
+			"Beacon ring",
+			"Mystic boots"
 		]
 	}
 ];
