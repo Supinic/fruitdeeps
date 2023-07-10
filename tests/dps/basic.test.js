086a5415cc861f36f2d651ced5b7a68ecd9ef8b5
Supinic <supinic@protonmail.com>
2023-07-11 00:42:58 +0200

    rewrote `Dps.setHitDist` to use new `HitFreqStore` API
    - included a new test for when NPC HP is lower than the player's maximum hit

--- tests/dps/basic.test.js
+++ tests/dps/basic.test.js
@@ -14,4 +14,16 @@ describe("DPS - basic", () => {
 		assertRounded(accuracy, 0.9578, 4);
 		assertRounded(dps, 2.195, 3);
 	});
+
+	it("Attacking a combat dummy with maxHit > HP", () => {
+		dummy.stats.hitpoints = 5;
+
+		const dpsInstance = new Dps({ monster: dummy, player: {} });
+		const result = dpsInstance.output();
+
+		const { accuracy, maxHit, dps } = result;
+		strictEqual(maxHit, 11);
+		assertRounded(accuracy, 0.9578, 4);
+		assertRounded(dps, 1.497, 3);
+	});
 });
